1bfbac80a2034feb33b20eeccadf466bee226e7f
bianloic <bian.loic@gmail.com>
2014-09-29 17:05:12 +0200

    add hibernate field

--- src/fr/adaming/awal/dao/interfaces/IRepairerDao.java
+++ src/fr/adaming/awal/dao/interfaces/IRepairerDao.java
@@ -5,10 +5,23 @@
  */
 package fr.adaming.awal.dao.interfaces;
 
+import fr.adaming.awal.entity.Repairer;
+import java.util.List;
+
 /**
  *
  * @author INTI0217
  */
 public interface IRepairerDao {
+    List<Repairer> getAll();
     
+    boolean create(Repairer address);
+    
+    boolean update(Repairer address);
+    
+    boolean delete(Repairer address);
+    
+    boolean delete(int id);
+    
+    Repairer getById(int id);    
 }
