c5ae3871313e9a61254d595f36836aa75e3fe38a
waelzamouri <waelzamouri@gmail.com>
2014-10-02 14:09:47 +0200

    add entity DeviceInsuranceModel -Idao

---
+++ src/fr/adaming/awal/dao/interfaces/IDeviceInsuranceModeleDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.dao.interfaces;
+
+import fr.adaming.awal.entity.Deviceinsurancemodel;
+
+/**
+ *
+ * @author INTI0227
+ */
+public interface IDeviceInsuranceModeleDao extends IDao<Deviceinsurancemodel>{
+    
+}
