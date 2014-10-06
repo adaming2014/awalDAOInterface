1fa05a0f870339960a6a47cf9383b141563c9961
bianloic <bian.loic@gmail.com>
2014-10-06 15:34:07 +0200

    add method

--- src/fr/adaming/awal/dao/interfaces/IDeviceInsuranceModeleDao.java
+++ src/fr/adaming/awal/dao/interfaces/IDeviceInsuranceModeleDao.java
@@ -6,11 +6,13 @@
 package fr.adaming.awal.dao.interfaces;
 
 import fr.adaming.awal.entity.Deviceinsurancemodel;
+import fr.adaming.awal.entity.Modele;
+import java.util.List;
 
 /**
  *
  * @author INTI0227
  */
 public interface IDeviceInsuranceModeleDao extends IDao<Deviceinsurancemodel>{
-    
+    List<Deviceinsurancemodel> getDeviceInsuranceModelsByModel(Modele modele);
 }
